41fe2171b80397a1cbc2894aa2df2dc689fe09a8
QMerrims <mmerrims@mail.bradley.edu>
2025-04-16 13:24:18 -0500

    Controls
    Added a temporary controls screen to the main menu

--- ClimateChangeGame/Assets/Scripts/MainMenu.cs
+++ ClimateChangeGame/Assets/Scripts/MainMenu.cs
@@ -5,6 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private GameObject ControlScreen;
+    [SerializeField] private GameObject Buttons;
+
     public void StartGame()
     {
         SceneManager.LoadScene(1);
@@ -23,8 +26,21 @@ public class MainMenu : MonoBehaviour
 
     public void Controls()
     {
-        //Not added yet since we don't have a controls scene yet
+        //Hi Cam! You don't need to modify anything in this script to get the controls screen working
+        //Just change the GameObjects in the scene. :3
         Debug.Log("Control Button Activated");
+        if (Buttons.activeSelf == true) 
+        {
+            ControlScreen.SetActive(true);
+            Buttons.SetActive(false);
+        }
+        else if (Buttons.activeSelf == false) 
+        {
+            ControlScreen.SetActive(false);
+            Buttons.SetActive(true);
+            
+        }
+
     }
 
     public void BackToMainMenu()
