916aa79194e735383c7047c4d5a2b6175ab99327
Hollow-Cube4444 <159060858+ACardamone4@users.noreply.github.com>
2025-05-01 19:24:19 -0500

    fixed big bullet issue

--- ClimateChangeGame/Assets/Scripts/BulletPlatformScript.cs
+++ ClimateChangeGame/Assets/Scripts/BulletPlatformScript.cs
@@ -4,13 +4,23 @@ public class BulletPlatformScript : MonoBehaviour
 {
     [SerializeField] private GameObject _spawnedObject;
     [SerializeField] private GameObject _parentBullet;
+    private bool stop;
+
+    private void Awake()
+    {
+        stop = false;
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Ground") || collision.gameObject.CompareTag("Oil") || collision.gameObject.CompareTag("GrassGround") || collision.gameObject.CompareTag("GrassWall") || collision.gameObject.layer == 11)
         {
-            GameObject SpawnPlatform = Instantiate(_spawnedObject, this.transform.position, this.transform.rotation);
-            Destroy(_parentBullet);
+            if (!stop)
+            {
+                stop = true;
+                GameObject SpawnPlatform = Instantiate(_spawnedObject, this.transform.position, this.transform.rotation);
+                Destroy(_parentBullet);
+            }
         }
     }
 }
