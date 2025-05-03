df3f5a11a2bb11696cf147a6582e98247924afbd
Hollow-Cube4444 <159060858+ACardamone4@users.noreply.github.com>
2025-05-03 13:44:48 -0500

    Player health commented

--- ClimateChangeGame/Assets/Scripts/PlayerHealth.cs
+++ ClimateChangeGame/Assets/Scripts/PlayerHealth.cs
@@ -1,7 +1,14 @@
-using TMPro;
+/*****************************************************************************
+// File Name :         PlayerHealth.cs
+// Author :            Amber C. Cardamone
+// Creation Date :     April 29th, 2025
+//
+// Brief Description : Checks the player's health, connects with a healthbar, and restarts the level if health goes to 0.
+*****************************************************************************/
+
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
@@ -10,26 +17,23 @@ public class PlayerHealth : MonoBehaviour
     [SerializeField] private float _oilDamage;
     [SerializeField] private float _groundHealing;
     [SerializeField] private float _grassHealing;
-    private PlayerMovement _playerMovement;
     [SerializeField] private bool grass;
     [SerializeField] private bool oil;
     [SerializeField] private bool grounded;
-    //[SerializeField] private GameObject _healthTextObject;
-    //[SerializeField] private TMP_Text _healthText;
-
     public Image healthBar;
+
+    /// <summary>
+    /// Sets up the health system to be resettable.
+    /// </summary>
     private void Start()
     {
         _health = _maxHealth;
-        _playerMovement = FindObjectOfType<PlayerMovement>();
-        //healthBar = FindObjectOfType<Image>();
-        //_healthTextObject = GameObject.Find("HealthText");
-        //if (_healthTextObject != null)
-        //{
-        //    _healthText = _healthTextObject.GetComponent<TMP_Text>();
-        //}
     }
 
+    /// <summary>
+    /// OnCollisionEnter Checks what the player is colliding with, and gives/loses health depending on what they hit.
+    /// </summary>
+    /// <param name="collision"></param>
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.layer == 13)
@@ -49,31 +53,30 @@ public class PlayerHealth : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Used for continuous hitting effects, checking if the player is hitting oil or grass.
+    /// </summary>
+    /// <param name="collision"></param>
     private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.gameObject.layer == 11)
         {
-            print("A");
             grass = false;
             oil = true;
             grounded = true;
         }
         else if (collision.gameObject.layer == 12)
         {
-            print("C");
             grass = true;
             oil = false;
             grounded = true;
         }
-        //else if (collision.gameObject.layer == 14)
-        //{
-        //    print("B");
-        //    grass = false;
-        //    oil = false;
-        //    grounded = true;
-        //}
     }
 
+    /// <summary>
+    /// Checks if the player leaves tiles
+    /// </summary>
+    /// <param name="collision"></param>
     private void OnCollisionExit2D(Collision2D collision)
     {
         if (collision.gameObject.layer == 10 || collision.gameObject.layer == 11 || collision.gameObject.layer == 12 || collision.gameObject.layer == 14)
@@ -92,6 +95,10 @@ public class PlayerHealth : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Checks if the player leaves tiles but a trigger this time.
+    /// </summary>
+    /// <param name="collision"></param>
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.gameObject.layer == 10 || collision.gameObject.layer == 11 || collision.gameObject.layer == 12 || collision.gameObject.layer == 14)
@@ -110,10 +117,11 @@ public class PlayerHealth : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Makes the player consistently lose/gain health, and checks if they have no health, reloads scene.
+    /// </summary>
     private void Update()
     {
-
-        //_healthText.text = ("" + _health);
         if (healthBar != null)
         {
             healthBar.fillAmount = _health / _maxHealth;
